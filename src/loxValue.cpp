50beef9bc73928c7d6d31c2d6c569652b4728308
Łukasz Niedzielski <luk20001122@gmail.com>
2023-01-18 09:01:04 +0100

    `LoxValue`: impl `getName`, use `bool`, update `<<`

--- src/loxValue.cpp
+++ src/loxValue.cpp
@@ -9,14 +9,20 @@ std::ostream &operator<<(std::ostream &strm, const LoxValue &value) {
                  [&strm](const std::string &string) -> std::ostream & {
                      return strm << string;
                  },
-                 [&strm](const LoxTrue&) -> std::ostream & {
-                     return strm << "True";
+                 [&strm](const bool &value) -> std::ostream & {
+                     return strm << (value ? "True" : "False");
                  },
-                 [&strm](const LoxFalse&) -> std::ostream & {
-                     return strm << "False";
-                 },
-                 [&strm](const LoxNil&) -> std::ostream & {
+                 [&strm](const LoxNil &) -> std::ostream & {
                      return strm << "Nil";
                  }},
         value);
 };
+const char *LoxValue::getName() const {
+    // clang-format off
+    return std::visit( 
+    Overload{ [](const double &number) -> auto { return "Number"; }
+            , [](const std::string &string) -> auto{ return "String"; } 
+            , [](const bool &value) -> auto{ return "Boolean"; }
+            , [](const LoxNil &) -> auto{ return "Null"; }
+    }, *this); // clang-format on
+}
--- src/loxValue.h
+++ src/loxValue.h
@@ -1,16 +1,22 @@
-#ifndef LOXVALUE
-#define LOXVALUE
-
 #include "overload.h"
 #include <string>
 #include <variant>
 
-struct LoxTrue {};
-struct LoxFalse {};
+#ifndef LOXVALUE
+#define LOXVALUE
+
 struct LoxNil {};
 
-struct LoxValue : std::variant<double, std::string, LoxTrue, LoxFalse, LoxNil> {
+struct LoxValue : std::variant<double, std::string, bool, LoxNil> {
     using variant::variant;
+
+    template <typename T> T into() const { return std::get<T>(*this); }
+
+    template <typename T> bool is() const {
+        return std::holds_alternative<T>(*this);
+    }
+
+    const char *getName() const;
 };
 
 std::ostream &operator<<(std::ostream &strm, const LoxValue &value);
